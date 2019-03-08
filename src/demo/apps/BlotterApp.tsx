c0631804d73ddb008b22f36c9c726891fb5f2703
Theo Ottah <48205674+TheoOttahOF@users.noreply.github.com>
2019-03-08 15:41:46 +0000

    Simplified blotter app

--- src/demo/apps/BlotterApp.tsx
+++ src/demo/apps/BlotterApp.tsx
@@ -1,50 +1,34 @@
 import * as React from 'react';
-
 import '../../../res/demo/css/w3.css';
-
 import { SymbolsTable } from '../components/blotter/SymbolsTable';
-
-interface AppState {
-    symbols: Symbol[];
-}
 
 export interface Symbol {
     name: string;
 }
 
-const initialState: AppState = {
-    symbols: [
-        {"name":"AAPL"},
-        {"name":"AMD"},
-        {"name":"AMZN"},
-        {"name":"CMCSA"},
-        {"name":"CSCO"},
-        {"name":"FB"},
-        {"name":"GOOG"},
-        {"name":"INTC"},
-        {"name":"MSFT"},
-        {"name":"NFLX"},
-        {"name":"NVDA"},
-        {"name":"PYPL"},
-        {"name":"QCOM"},
-        {"name":"ROKU"},
-        {"name":"TSLA"}
-    ]
-};
+const symbols:  Symbol[] = [
+    {"name":"AAPL"},
+    {"name":"AMD"},
+    {"name":"AMZN"},
+    {"name":"CMCSA"},
+    {"name":"CSCO"},
+    {"name":"FB"},
+    {"name":"GOOG"},
+    {"name":"INTC"},
+    {"name":"MSFT"},
+    {"name":"NFLX"},
+    {"name":"NVDA"},
+    {"name":"PYPL"},
+    {"name":"QCOM"},
+    {"name":"ROKU"},
+    {"name":"TSLA"}
+];
 
-export class BlotterApp extends React.Component<{}, AppState> {
-    constructor(props: {}) {
-        super(props);
-        
+// tslint:disable-next-line:variable-name
+export const BlotterApp: React.FunctionComponent = () =>{
+    React.useEffect(() =>{
         document.title = "Blotter";
+    });
 
-        //Initial set of symbols is hard-coded.
-        this.state = initialState;
-    }
-
-    public render(): JSX.Element {
-        return (
-            <SymbolsTable items={this.state.symbols} />
-        );
-    }
-}
+    return (<SymbolsTable items={symbols}/>);
+};
