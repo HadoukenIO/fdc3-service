a3706085e786ccab03aff1873ba9f7ed36a4d728
Theo Ottah <48205674+TheoOttahOF@users.noreply.github.com>
2019-03-11 17:56:55 +0000

    CallButton rewrite

--- src/demo/components/dialer/CallButton.tsx
+++ src/demo/components/dialer/CallButton.tsx
@@ -8,38 +8,16 @@ interface CallButtonProps {
     handleClick?: () => void;
 }
 
-interface CallButtonState{
-    inCall: boolean;
-    canCall: boolean;
-}
-
-export class CallButton extends React.Component<CallButtonProps, CallButtonState> {
-    constructor(props: CallButtonProps) {
-        super(props);
-
-        this.state = {
-            inCall: props.inCall || false,
-            canCall: props.canCall || false
-        };
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    public render(): JSX.Element {
-        const {inCall} = this.props;
-        const isEnabled: boolean = inCall || this.props.canCall;
-
-        return (
-            <button className={"call-btn w3-button " + (inCall ? "w3-red" : "w3-green")} disabled={!isEnabled} onClick={this.handleClick}>{inCall ? "End Call" : "Call"}</button>
-        );
-    }
-
-    private handleClick(event: React.MouseEvent<HTMLButtonElement>): void {
-        const {canCall, inCall} = this.props;
-        const handler: (()=>void)|undefined = this.props.handleClick;
-
+// tslint:disable-next-line:variable-name
+export const CallButton: React.FunctionComponent<CallButtonProps> = (props) => {
+    const {handleClick: handler, canCall, inCall} = props;
+    const isEnabled: boolean = inCall || canCall;
+    const handleClick = () => {
         if (handler && (inCall || canCall)) {
             handler();
         }
-    }
-}
+    };
+    return (
+        <button className={"call-btn w3-button " + (inCall ? "w3-red" : "w3-green")} disabled={!isEnabled} onClick={handleClick}>{inCall ? "End Call" : "Call"}</button>
+    );
+};
