fdbac2d56d08a8540eef214bb0a886615dd4bc5c
Theo Ottah <48205674+TheoOttahOF@users.noreply.github.com>
2019-03-11 18:00:53 +0000

    Call timer

--- src/demo/components/dialer/CallTimer.tsx
+++ src/demo/components/dialer/CallTimer.tsx
@@ -2,50 +2,29 @@ import * as React from 'react';
 
 import './CallTimer.css';
 
-interface CallTimerState {
-    timer: number|null;
-    counter: number;
-}
+// tslint:disable-next-line:variable-name
+export const CallTimer: React.FunctionComponent = () => {
+    const [timer, setTimer] = React.useState();
+    const [counter, setCounter] = React.useState(0);
+    const seconds: number = (counter % 60),
+        minutes: number = Math.floor(counter / 60) % 60,
+        hours: number = Math.floor(counter / 3600);
+    const tick = () => {
+        setCounter(counter + 1);
+    };
 
-export class CallTimer extends React.Component<{}, CallTimerState> {
-    constructor(props: {}) {
-        super(props);
+    React.useEffect(() => {
+        setTimer(setInterval(tick, 1000));
 
-        this.state = {
-            timer: null,
-            counter: 0
+        //Cleanup
+        return () => {
+            clearInterval(timer);
         };
+    }, []);
 
-        this.tick = this.tick.bind(this);
-    }
-
-    public componentDidMount() {
-        const timer: number = setInterval(this.tick, 1000) as unknown as number;
-        this.setState({timer, counter: 0});
-    }
-
-    public componentWillUnmount() {
-        clearInterval(this.state.timer||undefined);
-        this.setState({timer: null, counter: this.state.counter});
-    }
-
-    public render(): JSX.Element {
-        const time: number = this.state.counter;
-
-        const seconds: number = (time % 60),
-            minutes: number = Math.floor(time / 60) % 60,
-            hours: number = Math.floor(time / 3600);
-
-        return (
-            <div className="call-timer w3-text-green w3-xlarge w3-center">
-                {hours < 10 ? "0" + hours : hours} : {minutes < 10 ? "0" + minutes : minutes} : {seconds < 10 ? "0" + seconds : seconds}
-            </div>
-        );
-    }
-
-    private tick() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    }
-}
+    return (
+        <div className="call-timer w3-text-green w3-xlarge w3-center">
+            {hours < 10 ? "0" + hours : hours} : {minutes < 10 ? "0" + minutes : minutes} : {seconds < 10 ? "0" + seconds : seconds}
+        </div>
+    );
+};
