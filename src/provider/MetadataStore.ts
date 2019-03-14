50bc5ed65e0cd50dd465fe0ef137ec47e41c26ff
Tom Roberts <troberts@openfin.co>
2019-03-14 14:31:36 +0000

    Add slightly meatier tests to test the test runner

--- src/provider/MetadataStore.ts
+++ src/provider/MetadataStore.ts
@@ -104,6 +104,9 @@ export class MetadataStore {
      * @param app An OpenFin application that has been created from 'appData'
      */
     public update(appData: IApplication, app: Application): void {
+        if (typeof appData.id !== 'number') {
+            throw new TypeError('Invalid appData passed to MetadataStore. "id" must be a number.');
+        }
         if (!this.appData.hasOwnProperty(appData.id)) {
             this.appData[appData.id] = {uuid: app.identity.uuid, name: app.identity.name!, directoryId: appData.id};
         }
