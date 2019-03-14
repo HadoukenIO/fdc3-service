1fd8140508d767b18ca32cd82b56136a0e7de2b5
Tom Roberts <troberts@openfin.co>
2019-03-14 17:07:15 +0000

    Added basic runner which spins up the environment  - Still need to make it run tests  - also needs tests to run

---
+++ test/runner.js
@@ -0,0 +1,69 @@
+const execa = require('execa');
+const os = require('os');
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const {launch} = require('hadouken-js-adapter');
+
+let port;
+const debugMode = true;
+
+const cleanup = async res => {
+    if (os.platform().match(/^win/)) {
+        const cmd = 'taskkill /F /IM openfin.exe /T';
+        execa.shellSync(cmd);
+    } else {
+        const cmd = `lsof -n -i4TCP:${port} | grep LISTEN | awk '{ print $2 }' | xargs kill`;
+        execa.shellSync(cmd);
+    }
+    process.exit((res.failed===true) ? 1 : 0);
+}
+
+const fail = err => {
+    console.error(err);
+    process.exit(1);
+}
+
+const run = (...args) => {
+    const p = execa(...args)
+    p.stdout.pipe(process.stdout)
+    p.stderr.pipe(process.stderr)
+    return p
+}
+
+/**
+ * Performs a clean build of the application and tests
+ */
+async function build() {
+    await run('npm', ['run', 'clean']);
+    await run('npm', ['run', debugMode ? 'build:dev' : 'build']);
+}
+
+async function serve() {
+    return new Promise((resolve) => {
+        const app = express();
+
+        // Sneakily return the test directory instead of the default one
+        app.get('/provider/app-directory.json', (req, res) => {
+            let testDirectory = JSON.parse(fs.readFileSync(path.join('res', 'test','app-directory.json')));
+
+            res.contentType('application/json');
+            res.json(testDirectory);
+        });
+
+        app.use(express.static('dist'));
+        app.use(express.static('res'));
+
+        app.listen(3923, resolve);
+    });
+}
+
+build()
+    .then(() => serve())
+    .then(async () => {
+        port = await launch({manifestUrl: 'http://localhost:3923/test/test-app-main.json'});
+        console.log('Openfin running on port ' + port);
+        return port
+    })
+    .catch(fail)
