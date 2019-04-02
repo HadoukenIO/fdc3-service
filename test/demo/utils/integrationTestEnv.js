504f4b931467273bb50d245f7d6a99fa12087ee9
Tom Roberts <troberts@openfin.co>
2019-04-02 13:22:06 +0100

    Linting in testEnv setup file

--- test/demo/utils/integrationTestEnv.js
+++ test/demo/utils/integrationTestEnv.js
@@ -1,39 +1,40 @@
 // Had problems with this as a typescript file, so leaving as js for now
-const NodeEnvironment = require('jest-environment-node');
 const fs = require('fs');
-const path = require('path');
-const puppeteer = require('puppeteer');
 const os = require('os');
+const path = require('path');
+
+const NodeEnvironment = require('jest-environment-node');
+const puppeteer = require('puppeteer');
 
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
 class PuppeteerEnvironment extends NodeEnvironment {
-  constructor(config) {
-    super(config);
-  }
-
-  async setup() {
-    await super.setup();
-    // get the wsEndpoint
-    const wsEndpoint = fs.readFileSync(path.join(DIR, 'wsEndpoint'), 'utf8');
-    if (!wsEndpoint) {
-      throw new Error('wsEndpoint not found');
+    constructor(config) {
+        super(config);
     }
 
-    // connect to puppeteer
-    this.global.__BROWSER__ = await puppeteer.connect({
-      browserWSEndpoint: wsEndpoint,
-    });
-  }
+    async setup() {
+        await super.setup();
+        // get the wsEndpoint
+        const wsEndpoint = fs.readFileSync(path.join(DIR, 'wsEndpoint'), 'utf8');
+        if (!wsEndpoint) {
+            throw new Error('wsEndpoint not found');
+        }
 
-  async teardown() {
-    await this.global.__BROWSER__.disconnect();
-    await super.teardown();
-  }
+        // connect to puppeteer
+        this.global.__BROWSER__ = await puppeteer.connect({
+            browserWSEndpoint: wsEndpoint
+        });
+    }
 
-  runScript(script) {
-    return super.runScript(script);
-  }
+    async teardown() {
+        await this.global.__BROWSER__.disconnect();
+        await super.teardown();
+    }
+
+    runScript(script) {
+        return super.runScript(script);
+    }
 }
 
 module.exports = PuppeteerEnvironment;
