6aab006dcd5f131f93aa9162feefb607604a6c2a
Theo Ottah <48205674+TheoOttahOF@users.noreply.github.com>
2019-04-18 13:32:58 +0100

    Fetch data

--- src/provider/model/AppDirectory.ts
+++ src/provider/model/AppDirectory.ts
@@ -8,6 +8,9 @@ enum StorageKeys {
     APPLICATIONS = 'applications'
 }
 
+// Demo development app directory url
+const devURL = 'http://localhost:3923/provider/sample-app-directory.json';
+
 
 @injectable()
 export class AppDirectory {
@@ -18,17 +21,22 @@ export class AppDirectory {
     public constructor() {
         this._store = localForage.createInstance({
             name: 'FDC3',
+            version: 1.0,
             storeName: 'app-directory'
         });
         // Set default values
         this._store.getItem<string>(StorageKeys.URL, (error, value) => {
-            this.updateURL(value);
+            // Use dev hardcoded url
+            if (value === null) {
+                value = devURL;
+            }
+            this._URL = value;
         });
         this._store.getItem<Application[]>(StorageKeys.APPLICATIONS, (error, value) => {
             if (value === null) {
                 value = [];
             }
-            this.updateDirectory(value);
+            this._directory = value;
         });
     }
 
@@ -36,8 +44,8 @@ export class AppDirectory {
      * Update the application directory in memory and storage.
      * @param applications To place into the directory.
      */
-    private async updateDirectory(applications: Application[] = []): Promise<void> {
-        await this._store.setItem(StorageKeys.APPLICATIONS, applications);
+    private async updateDirectory(applications: Application[]): Promise<void> {
+        await this._store.setItem<Application[]>(StorageKeys.APPLICATIONS, applications);
         this._directory = applications;
     }
 
@@ -46,46 +54,65 @@ export class AppDirectory {
      * @param url Directory URL.
      */
     private async updateURL(url: string) {
-        await this._store.setItem(StorageKeys.URL, url);
+        await this._store.setItem<string>(StorageKeys.URL, url);
         this._URL = url;
     }
 
     public async getAppByName(name: string): Promise<Application | null> {
-        await this.fetchData().catch(console.error);
+        await this.refreshDirectory();
         return this._directory.find((app: Application) => {
             return app.name === name;
         }) || null;
     }
 
     public async getAppsByIntent(intentType: string): Promise<Application[]> {
-        await this.fetchData().catch(console.error);
+        await this.refreshDirectory();
         return this._directory.filter((app: Application) => {
             return app.intents && app.intents.some(intent => intent.name === intentType);
         });
     }
 
     public async getAllApps(): Promise<Application[]> {
-        await this.fetchData().catch(console.error);
+        await this.refreshDirectory();
         return this._directory;
     }
 
-    private async fetchData(): Promise<void> {
-        const storedURL = await localForage.getItem('url');
-        // If using the demo app directory in production, return an empty array.
+    /**
+     * Fetch the AppDirectory.
+     * @param url Location of the AppDirectory.
+     */
+    private async fetchData(url: string): Promise<Application[]> {
+        const response: Response = await fetch(url);
+        if (response.ok) {
+            return response.json();
+        } else {
+            throw new Error('Error fetching app directory: ' + response.statusText);
+        }
+    }
+
+    /**
+     * Refresh the AppDirectory.
+     */
+    private async refreshDirectory(): Promise<void> {
+        // If using the demo app directory in production, set an empty array.
         if (process.env.NODE_ENV === 'production') {
             this.updateDirectory([]);
-        } else {
-            const URL = 'http://localhost:3923/provider/sample-app-directory.json';
-            const response: Response = await fetch(URL);
-            if (response.ok) {
-                const applications = await response.json();
-                this.updateDirectory(applications);
-            } else {
-                // Do not use stored data if the URL is different that the one stored
-                if (storedURL !== this._URL) {
-                    this.updateDirectory([]);
+            return;
+        }
+        const storedURL = await localForage.getItem<string>('url');
+        const currentURL = this._URL;
+        // Only update the directory if the URL has changed
+        // or the directory is empty (the last request may have failed?)
+        if (currentURL !== storedURL || (currentURL === storedURL && this._directory.length === 0)) {
+            try {
+                const applications = await this.fetchData(currentURL);
+                await this.updateDirectory(applications);
+                if (currentURL !== storedURL) {
+                    await this.updateURL(currentURL);
                 }
-                throw new Error('Error fetching app directory: ' + response.statusText);
+            } catch (error) {
+                // Dont update
+                return;
             }
         }
     }
--- test/provider/AppDirectory.unittest.ts
+++ test/provider/AppDirectory.unittest.ts
@@ -5,27 +5,69 @@ import {AppDirectory} from '../../src/provider/model/AppDirectory';
 import {Application} from '../../src/client/directory';
 
 
-describe('Storage', () => {
+enum StorageKeys {
+    URL = 'url',
+    APPLICATIONS = 'applications'
+}
+
+const appTemplate: Application = {
+    appId: 'id',
+    name: 'test-app',
+    manifest: '',
+    manifestType: ''
+};
+
+// Generate applications
+const generateApplications = (amount: number): Application[] => {
+    return Array(amount).fill(appTemplate).map((app, i) => ({
+        ...app,
+        appId: `id-${i}`,
+        name: `${app.name}-${i}`
+    }));
+};
+
+describe('AppDirectory Unit Tests', () => {
     let appDirectory: AppDirectory;
     let store: LocalForage;
-    beforeEach(async () => {
-        appDirectory = new AppDirectory();
+
+    describe('Storage', () => {
+        beforeEach(async () => {
+            appDirectory = new AppDirectory();
+        });
+
+        afterEach(async () => {
+            await store.clear();
+        });
+
+        it('Correctly sets up the store', async () => {
+            store = appDirectory['_store'];
+            // await store.ready();
+            const url = await store.getItem(StorageKeys.URL);
+            const applications = await store.getItem(StorageKeys.APPLICATIONS);
+            expect(url).toEqual(null);
+            expect(applications).toEqual(null);
+        });
+
+        describe('Storing fetchData', () => {
+            it('Correctly sets applications in the store', async () => {
+                appDirectory['fetchData'] = jest.fn(() => Promise.resolve([]));
+                await appDirectory.getAllApps();
+                const applications = await store.getItem(StorageKeys.APPLICATIONS);
+                expect(applications).toEqual([]);
+            });
+
+            it('Correctly stores fetchData in the store and in memory', async () => {
+                const fakeApps = generateApplications(3);
+                appDirectory['fetchData'] = jest.fn(() => Promise.resolve(fakeApps));
+                const returnedApplications = await appDirectory.getAllApps();
+                const storedApplications = await store.getItem(StorageKeys.APPLICATIONS);
+                expect(storedApplications).toEqual(fakeApps);
+                expect(returnedApplications).toEqual(fakeApps);
+            });
+        });
     });
 
-    afterEach(async () => {
-        await store.clear();
-    });
 
-    it('Correctly sets up the store', async () => {
-        store = appDirectory['_store'];
-        await store.ready();
-        const url = await store.getItem('url');
-        const applications = await store.getItem('applications');
-        expect(url).toEqual(null);
-        expect(applications).toEqual([]);
-    });
-
-    it('Stores applications from the app directory path', async () => {
-
+    describe('', () => {
     });
 });
