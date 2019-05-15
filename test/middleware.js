631ce7409b8ce7cdddd93261da58e709b21f8fa8
ryanpetersonOF <ryan@openfin.co>
2019-05-15 09:27:22 +0100

    [NA] Rename Middleware

--- test/middleware.js
+++ test/middleware.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-exports.default = (app) => {
+module.exports = (app) => {
     // Sneakily return the test directory instead of the default one
     app.get('/provider/sample-app-directory.json', (req, res) => {
         const testDirectory = JSON.parse(fs.readFileSync(path.join('res', 'test', 'sample-app-directory.json')));
