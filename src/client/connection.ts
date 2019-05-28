339c54e1b3a72a0756081b8b2f980671b237d489
bryangaleOF <bryan@openfin.co>
2019-05-28 17:35:00 +0100

    [SERVICE-478] Refine getServicePromise

--- src/client/connection.ts
+++ src/client/connection.ts
@@ -49,11 +49,11 @@ if (typeof fin !== 'undefined') {
 export function getServicePromise(): Promise<ChannelClient> {
     if (!channelPromise) {
         if (typeof fin === 'undefined') {
-            channelPromise = Promise.reject<ChannelClient>(new Error('Trying to connect to provider from provider'));
-        } else if (fin.Window.me.uuid === SERVICE_IDENTITY.uuid || fin.Window.me.name === SERVICE_IDENTITY.name) {
+            channelPromise = Promise.reject(new Error('fin is not defined. The openfin-fdc3 module is only intended for use in an OpenFin application.'));
+        } else if (fin.Window.me.uuid === SERVICE_IDENTITY.uuid && fin.Window.me.name === SERVICE_IDENTITY.name) {
             // Currently a runtime bug when provider connects to itself. Ideally the provider would never import a file
             // that includes this, but for now it is easier to put a guard in place.
-            channelPromise = Promise.reject(new Error('fin is not defined. The openfin-fdc3 module is only intended for use in an OpenFin application.'));
+            channelPromise = Promise.reject<ChannelClient>(new Error('Trying to connect to provider from provider'));
         } else {
             channelPromise = fin.InterApplicationBus.Channel.connect(SERVICE_CHANNEL, {payload: {version: PACKAGE_VERSION}}).then((channel: ChannelClient) => {
                 // Register service listeners
