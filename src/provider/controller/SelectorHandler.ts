24d59132ff0bdccf02b1cedb67d20bb62b592a30
Fran Rodriguez <f.x.rodriguez@hotmail.com>
2019-06-04 17:19:43 +0100

    [NA] Standardize on constructor injection (#102)

--- src/provider/controller/SelectorHandler.ts
+++ src/provider/controller/SelectorHandler.ts
@@ -69,14 +69,20 @@ export interface SelectorResult {
 export class SelectorHandler extends AsyncInit {
     private static SELECTOR_NAME: string = 'fdc3-selector';
 
-    @inject(Inject.APP_DIRECTORY)
-    private _directory!: AppDirectory;
-
-    @inject(Inject.MODEL)
-    private _model!: Model;
+    private readonly _directory: AppDirectory;
+    private readonly _model: Model;
 
     private _window!: _Window;
     private _channel!: ChannelClient;
+
+    constructor(
+        @inject(Inject.APP_DIRECTORY) directory: AppDirectory,
+        @inject(Inject.MODEL) model: Model,
+    ) {
+        super();
+        this._directory = directory;
+        this._model = model;
+    }
 
     /**
      * Performs one-off initialisation
