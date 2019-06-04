593cb8acbab2920da39de348d16e199a5e9840c1
ryanpetersonOF <ryan@openfin.co>
2019-06-04 13:12:09 +0100

    [SERVICE-462] Remove css import

--- src/provider/view/Resolver.tsx
+++ src/provider/view/Resolver.tsx
@@ -3,5 +3,4 @@ import {render} from 'react-dom';
 
 import {AppList} from './components/AppList';
 
-import '../../../res/provider/ui/resolver/css/resolver.css';
 render(<AppList />, document.getElementById('react-app'));
