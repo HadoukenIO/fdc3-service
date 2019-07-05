994e3506414e537626275bbaaf65cb81be6baa10
ryanpetersonOF <ryan@openfin.co>
2019-07-05 11:15:02 +0100

    [SERVICE-540] Webpack Integrations

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,41 @@
 const path = require('path');
 const outputDir = path.resolve(__dirname, './dist');
 
+const {SchemaToDefaultsPlugin} = require('openfin-service-config/plugins/SchemaToDefaultsPlugin');
+const {SchemaToTypeScriptPlugin} = require('openfin-service-config/plugins/SchemaToTypeScriptPlugin');
 /**
  * Import the webpack tools from openfin-service-tooling
  */
 const webpackTools = require('openfin-service-tooling').webpackTools;
+
+const schemaRoot = path.resolve(__dirname, './res/provider/config');
+const schemaOutput = path.resolve(__dirname, './gen/provider/config');
+const defaultsOutput = path.resolve(__dirname, './gen/provider/config/defaults.json');
+
+/**
+ * Generate TypeScript definition files from the config schema files.
+ *
+ * Generated code is placed inside a top-level 'gen' folder, whose structure mirrors that of
+ * the 'src', 'res' and 'test' folders.
+ */
+const schemaDefaultsPlugin = new SchemaToDefaultsPlugin({
+    outputPath: defaultsOutput,
+    input: `${schemaRoot}/fdc3-config.schema.json`
+});
+
+/**
+ * Generate TypeScript definition files from the config schema files.
+ *
+ * Generated code is placed inside a top-level 'gen' folder, whose structure mirrors that of
+ * the 'src', 'res' and 'test' folders.
+ */
+const schemaTypesPlugin = new SchemaToTypeScriptPlugin({
+    schemaRoot,
+    outputPath: schemaOutput,
+    input: [
+        `${schemaRoot}/fdc3-config.schema.json`
+    ]
+});
 
 /**
  * Modules to be exported
@@ -14,7 +45,7 @@ module.exports = [
     webpackTools.createConfig(`${outputDir}/client`, './src/client/main.ts', {
         minify: true, isLibrary: true, libraryName: 'fdc3', outputFilename: 'openfin-fdc3'
     }, webpackTools.versionPlugin),
-    webpackTools.createConfig(`${outputDir}/provider`, './src/provider/index.ts', undefined, webpackTools.manifestPlugin, webpackTools.versionPlugin),
+    webpackTools.createConfig(`${outputDir}/provider`, './src/provider/index.ts', undefined, webpackTools.manifestPlugin, webpackTools.versionPlugin, schemaDefaultsPlugin, schemaTypesPlugin),
     webpackTools.createConfig(`${outputDir}/provider/ui`, {
         'resolver': './src/provider/view/Resolver.tsx'
     }, undefined, webpackTools.versionPlugin),
