b3d28bad33aa8754d9b1017b5c83875e91854197
Tom Roberts <troberts@openfin.co>
2019-07-17 13:58:38 +0100

    Force apiConnection behind windowCreated handler

--- src/provider/model/Model.ts
+++ src/provider/model/Model.ts
@@ -8,6 +8,7 @@ import {ChannelId, DEFAULT_CHANNEL_ID} from '../../client/main';
 import {APIHandler} from '../APIHandler';
 import {APIFromClientTopic} from '../../client/internal';
 import {DESKTOP_CHANNELS} from '../constants';
+import {deferredPromise} from '../utils/async';
 
 import {AppWindow} from './AppWindow';
 import {ContextChannel, DefaultContextChannel, DesktopContextChannel} from './ContextChannel';
@@ -34,6 +35,8 @@ export class Model {
     private readonly _channelsById: {[id: string]: ContextChannel};
 
     private readonly _onWindowRegisteredInternal: Signal0 = new Signal0();
+
+    private readonly _pendingRegistrations: Map<string, Promise<void>> = new Map();
 
     constructor(
         @inject(Inject.APP_DIRECTORY) directory: AppDirectory,
@@ -128,21 +131,32 @@ export class Model {
     }
 
     private async onWindowCreated(identity: Identity, manifestUrl: string): Promise<void> {
+        // Registration is asynchronous and sensitive to race conditions. We use a deferred promise
+        // to signal to other sensitive operations that it is safe to proceed.
+        const [pendingRegistration, clearPending] = deferredPromise();
+        this._pendingRegistrations.set(getId(identity), pendingRegistration);
+
         const apps = await this._directory.getAllApps();
         const appInfoFromDirectory = apps.find(app => app.manifest.startsWith(manifestUrl));
 
+        const id: string = getId(identity);
         if (!appInfoFromDirectory) {
             // If the app is not in directory we ignore it. We'll add it to the model if and when it connects to FDC3
+            clearPending();
+            this._pendingRegistrations.delete(getId(identity));
             return;
-        }
-
-        const id: string = getId(identity);
-        if (this._windowsById[id]) {
+        } else if (this._windowsById[id]) {
             console.info(`Ignoring window created event for ${id} - window was already registered`);
+            clearPending();
+            this._pendingRegistrations.delete(getId(identity));
             return;
+        } else {
+            this.registerWindow(appInfoFromDirectory, identity, true);
         }
 
-        this.registerWindow(appInfoFromDirectory, identity, true);
+        // Registration finished, allow any other sensitive operations to proceed
+        clearPending();
+        this._pendingRegistrations.delete(getId(identity));
     }
 
     private onWindowClosed(identity: Identity): void {
@@ -158,6 +172,12 @@ export class Model {
     }
 
     private async onApiHandlerConnection(identity: Identity): Promise<void> {
+        // Wait for windowCreated handler to finish to avoid race conditions that
+        // can occur when these two run "concurrently"
+        if (this._pendingRegistrations.has(getId(identity))) {
+            await this._pendingRegistrations.get(getId(identity));
+        }
+
         const appWindow = this.getWindow(identity);
 
         // Window is not in model - this should mean that the app is not in the directory, as directory apps are immediately added to model upon window creation
