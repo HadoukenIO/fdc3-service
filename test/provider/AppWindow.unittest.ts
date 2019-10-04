a608a22cab03ef871bf662c9efd58577c1994042
Tom Roberts <troberts@openfin.co>
2019-10-04 16:29:01 +0100

    Update AppWindowtests

--- test/provider/AppWindow.unittest.ts
+++ test/provider/AppWindow.unittest.ts
@@ -1,40 +1,28 @@
 import 'reflect-metadata';
 
-import {_Window} from 'openfin/_v2/api/window/window';
 import {Identity} from 'openfin/_v2/main';
 
 import {FinAppWindow} from '../../src/provider/model/FinAppWindow';
 import {createMockChannel} from '../mocks';
 import {Application} from '../../src/client/main';
+import {AbstractAppWindow} from '../../src/provider/model/AppWindow';
+import {ContextChannel} from '../../src/provider/model/ContextChannel';
 
-// Fake fin object exposing only the functionality used by FinAppWindow
-const fin = {
-    Window: {
-        wrapSync: (identity: Identity): _Window => createMockFinWindow(identity)
+class TestAppWindow extends AbstractAppWindow {
+    private readonly _identity: Readonly<Identity>;
+
+    constructor(identity: Identity, appInfo: Application, channel: ContextChannel, creationTime: number | undefined, appWindowNumber: number) {
+        super(identity, appInfo, channel, creationTime, appWindowNumber);
+
+        this._identity = identity;
     }
-};
 
-beforeAll(() => {
-    // Expose fake fin in the global scope for imports
-    Object.assign(global, {fin});
-});
-afterAll(() => {
-    // Remove fake fin from global scope when we're done
-    Object.assign(global, {fin: undefined});
-});
+    public get identity() {
+        return this._identity;
+    }
 
-beforeEach(() => {
-    jest.resetAllMocks();
-});
-
-// Creates a minimal stub of an openfin Window object containing only
-// the functionality used by FinAppWindow
-function createMockFinWindow(identity: Identity): jest.Mocked<_Window> {
-    return {
-        identity,
-        bringToFront: jest.fn<Promise<void>, []>(),
-        focus: jest.fn<Promise<void>, []>()
-    } as any as jest.Mocked<_Window>;
+    public bringToFront = jest.fn<Promise<void>, []>();
+    public focus = jest.fn<Promise<void>, []>();
 }
 
 const mockChannel = createMockChannel();
@@ -47,11 +35,15 @@ const fakeAppInfo: Application = {
     manifestType: 'openfin'
 };
 
+beforeEach(() => {
+    jest.resetAllMocks();
+});
+
 describe('When querying if a window has a context listener', () => {
-    let testAppWindow: FinAppWindow;
+    let testAppWindow: TestAppWindow;
 
     beforeEach(() => {
-        testAppWindow = new FinAppWindow(fakeIdentity, fakeAppInfo, mockChannel, Date.now(), 0);
+        testAppWindow = new TestAppWindow(fakeIdentity, fakeAppInfo, mockChannel, Date.now(), 0);
     });
 
     test('A freshly-initialized window returns false', () => {
