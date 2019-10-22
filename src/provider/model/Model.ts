c4ddf8d0b6e865f4647f2eeb85f76afe11fd0019
bryangaleOF <bryan@openfin.co>
2019-10-22 15:04:45 +0100

    Revert "[SERVICE-613] Remove redundant line"
    This reverts commit e889bd71547bf774befb59b45434a44fe5cd9312.

--- src/provider/model/Model.ts
+++ src/provider/model/Model.ts
@@ -322,6 +322,7 @@ export class Model {
 
         const {uuid} = identity;
         const liveApp = this._liveAppsByUuid[uuid];
+        this._liveAppsByUuid[uuid] = liveApp;
 
         // Only register windows once they are connected to the service
         allowReject(expectedWindow.connected.then(async () => {
