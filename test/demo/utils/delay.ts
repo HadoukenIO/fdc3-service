d7f020c9b4ac49fd67f142e3ed6d6c55e6c9c1f4
bryangaleOF <bryan@openfin.co>
2019-10-24 10:06:46 +0100

    [SERVICE-613] Add delay for handshake time

--- test/demo/utils/delay.ts
+++ test/demo/utils/delay.ts
@@ -11,5 +11,8 @@ export async function delay(duration: number) {
 export enum Duration {
     PAGE_RELOAD = 500,
     PAGE_NAVIGATE = 500,
-    WINDOW_REGISTRATION = 1000
+    WINDOW_REGISTRATION = 1000,
+    // Certain events involve a handshake between client and service, but the API call is unawaited on the client, so we use this delay to
+    // ensure the handshake has occured
+    LISTENER_HANDSHAKE = 250
 }
