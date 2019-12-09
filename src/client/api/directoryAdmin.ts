0d7e64669e7618e901925ac0735d7072044049ab
bryangaleOF <bryan@openfin.co>
2019-12-09 13:50:38 +0000

    [SERVICE-820] WIP API docs

--- src/client/api/directoryAdmin.ts
+++ src/client/api/directoryAdmin.ts
@@ -7,8 +7,8 @@
  * live on that domain.
  *
  * The app directory for a domain is specified or updated by calling the [[updateAppDirectory]] with a migration
- * handler callback. This callback will be called with an [[AppDirectory]] object which allows the current (possibly
- * empty) state of the app directory to the read and modified.
+ * handler callback provided by the application. This callback will be called with an [[AppDirectory]] object which
+ * allows the current (possibly empty) state of the app directory to the read and modified.
  *
  * Where app directories of different names provide applications of conflicting names, appIds, or UUIDs, the service
  * will arbitrarily choose one of the conflicting applications to include and discard the rest.
@@ -68,65 +68,100 @@ export interface UpdateAppDirectoryOptions {
      * The version of the app directory outputted by the migration handler. If exists, must be a positive integer,
      * and should be greater than or equal to `maxSourceVersion` if that also exists. If this is specified, and the
      * current version of the app directory is greater than this version, the migration handler will not be called. If
-     * this is not specied, the version of the app directory will be unchanged.
+     * this is not specied, the version of the app directory will be unchanged, or will be set to 1 when creating a new
+     * app directory.
      */
     destinationVersion?: number;
 }
 
 /**
- * Placeholder
+ * Interface for the object passed to the migration handler, to allow reading and modifying the current app directory.
+ * Note that storing this object and attempting to a call function on it beyond the duration of the migration handler
+ * will cause an error to be thrown.
  */
 export interface AppDirectory {
+    /**
+     * The current version of the app directory, or 0 if no app directory has yet been created.
+     */
     sourceVersion: number;
 
+    /**
+     * A [[DirectoryCollection]] object that allows reading an modifying remote snippets in the current app directory.
+     * Remote snippets are URLs that contain a JSON array of [[Application]]s to be included in the app directory.
+     * URLs not within the current appliction's domain will be ignored by the service.
+     */
     remoteSnippets: RemoteSnippetsDirectoryCollection;
+
+    /**
+     * A [[DirectoryCollection]] object that allows reading modifying stored applications in the current app directory.
+     * Stored applications are [[Application]]s stored locally.
+     */
     storedApplications: StoredApplicationDirectoryCollection;
 }
 
 /**
- * Placeholder
+ * Interface for reading an modifying remote snippets and stored applications. Also se
+ * e
+ * [[RemoteSnippetsDirectoryCollection]] and [[StoredApplicationDirectoryCollection]].
+ *
+ * @typeparam T The type stored by this collection
+ * @typeparam U An ID type that may be used to refer to an object stored by this collection
  */
 export interface DirectoryCollection<T, U = T> {
 
     /**
-     * Placeholder
+     * Values held by this collection before any modification has occured.
      */
     readonly source: ReadonlyArray<T>;
 
     /**
-     * Placeholder
+     * Adds either a single value or multiple values to this collection.
+     *
+     * @param arg The value or array of values to add
      */
     add: (arg: T | T[]) => void;
 
     /**
-     * Placeholder
+     * Removes either a single value or multiple values to this collection. The value or values to be removed may be
+     * specified by either a value of type `T` that is deep-equal to the one to be removed, or a value of type `U` that
+     * refers to the value to be removed (implementation specific to the implentation of this interface).
+     *
+     * @param arg The value or array of values, or ID of the value or array of IDs of the values, to remove
      */
     remove: (arg: T | T[] | U | U[]) => void;
 
     /**
-     * Placeholder
+     * Removes all values from this collection
      */
     removeAll: () => void;
 
     /**
-     * Placeholder
+     * Removes all values from this collection, and then populates it with the value or values provided.
+     *
+     * @param arg The value or array of values to populate this collection with
      */
     set: (arg: T | T[]) => void;
 }
 
 /**
- * Placeholder
+ * Inteface for reading and modifying remote snippets in the current app directory. Since these are specified as string
+ * there is no separate ID type.
  */
 export interface RemoteSnippetsDirectoryCollection extends DirectoryCollection<string, string> {
 
 }
 
 /**
- * Placeholder
+ * Interface for reading and modifying stored applications. When removing applications, applications can be refered to
+ * be either a complete, deep-equal [[Application]] object, or by the applications [[AppName]].
  */
 export interface StoredApplicationDirectoryCollection extends DirectoryCollection<Application, string> {
     /**
-     * Placeholder
+     * Adds the current application to the app directory.
+     *
+     * @param application A partial [[Application]] of any values that should override those derrived by the service
+     * @throws `Error` if the service is unable to determine the manifest URL for this application and it hasn't been
+     * given in the `application` paramter.
      */
     addSelf: (application?: Partial<Application>) => void;
 }
@@ -151,12 +186,22 @@ export interface StoredDirectoryShardMap {[key: string]: StoredDirectoryShard}
 export const APP_DIRECTORY_STORAGE_TAG: string = 'of-fdc3-service.directory';
 
 /**
- * Placeholder
+ * Updates the app directory for the current domain/specified namespace, using a migration handler function provided by
+ * the application. If no app directory has previously been written, the migration handler will be taken to be updating
+ * an 'empty' app directory with version 0.
  *
- * @param migrationHandler Placeholder
- * @param options Placeholder
+ * @param migrationHandler The migration handler function that should perform the update to the app directory for the
+ * current domain/specified namespace. This will be called with an [[AppDirectory]] object. Note that depending on
+ * `options`, and any other applications attempting to write to the app directory, this function may be called zero,
+ * one, or many times
+ * @param options An optional [[UpdateAppDirectoryOptions]] object to control the behaviour of the current update
+ * @throws `TypeError` if `migrationHandler` is not a function
+ * @throws `TypeError` if not either undefined or an object, or any of it's contained values do not meet requirements
+ * specified in [[UpdateAppDirectoryOptions]]
  */
 export async function updateAppDirectory(migrationHandler: UpdateAppDirectoryMigrationHandler, options?: UpdateAppDirectoryOptions): Promise<void> {
+    // TODO: type check options, check for empty strings, positive integer values.
+
     const selfApplication = await getSelfApplication();
 
     const namespaceKey = getNamespaceKey(options);
@@ -203,7 +248,7 @@ ${options.destinationVersion}. Skipping directory update.`);
         });
 
         const result: StoredDirectoryShard = {
-            version,
+            version: (options && options.destinationVersion !== undefined) ? options.destinationVersion : version === 0 ? 1 : version,
             remoteSnippets: remoteSnippets.build(),
             storedApplications: storedApplications.build()
         };
@@ -341,7 +386,7 @@ async function getSelfApplication(): Promise<Application> {
         startup_app: {uuid: string; name?: string; icon?: string};
     }
 
-    // TODO: Use latest version of this from develop FinEnvironment
+    // TODO: Use latest version of this from develop FinEnvironment. Throw exception if no able to determine manifest
     const {shortcut, startup_app: startupApp} = applicationInfo.manifest as OFManifest;
 
     const title = (shortcut && shortcut.name) || startupApp.name || startupApp.uuid;
