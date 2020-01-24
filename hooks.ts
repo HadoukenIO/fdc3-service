6478d359beb5076b738e074a6541d16a91138be9
bryangaleOF <bryan@openfin.co>
2020-01-24 12:20:17 +0000

    [N/A] Use more conventional import syntax

--- hooks.ts
+++ hooks.ts
@@ -1,5 +1,5 @@
-import fs = require('fs');
-import path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
 
 import {Hook, registerHook} from 'openfin-service-tooling/utils/allowHook';
 
