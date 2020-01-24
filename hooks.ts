178c86da0b25d896e4c0f064c537646b5ce552fe
bryangaleOF <47325366+bryangaleOF@users.noreply.github.com>
2020-01-24 13:14:41 +0000

    [N/A] Update service tooling (#205)

--- hooks.ts
+++ hooks.ts
@@ -1,10 +1,12 @@
-const fs = require('fs');
-const path = require('path');
+import * as fs from 'fs';
+import * as path from 'path';
 
-module.exports = (app) => {
+import {Hook, registerHook} from 'openfin-service-tooling/utils/allowHook';
+
+registerHook(Hook.TEST_MIDDLEWARE, (app) => {
     // Sneakily return the test directory instead of the default one
     app.get('/provider/sample-app-directory.json', (req, res) => {
-        const testDirectory = JSON.parse(fs.readFileSync(path.join('res', 'test', 'sample-app-directory.json')));
+        const testDirectory = JSON.parse(fs.readFileSync(path.join('res', 'test', 'sample-app-directory.json')).toString());
 
         res.contentType('application/json');
         res.json(testDirectory);
@@ -17,6 +19,4 @@ module.exports = (app) => {
         ms = parseInt(ms) || 0;
         setTimeout(() => res.status(200).send({delay: ms}), ms);
     });
-};
-
-
+});
