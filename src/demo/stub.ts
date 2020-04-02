7b34a8547a77128f636c622fb0245498dc2d321c
Philip Broadbent <p.j.broadbent@gmail.com>
2020-04-02 18:42:55 +0100

    [SERVICE-989] Cleaned-up stub.ts

--- src/demo/stub.ts
+++ src/demo/stub.ts
@@ -1,16 +1,15 @@
-// declare var window: Window & typeof globalThis & {fdc3?: typeof import('./main')};
-// declare global {
-//     interface Window {
-//         fdc3?: typeof import('./main');
-//     }
-// }
-
 type FDC3 = typeof import('../client/main');
+const windowWithFDC3: Window & {fdc3?: FDC3} = window;
 
-const tmp: Window & {fdc3?: FDC3} = window;
+/**
+ * Fetches the client library from the correct source.
+ *
+ * When using runtime injection: The runtime will inject a script that defines an `fdc3` global variable
+ * When using desktop services: The client is imported, typically from an NPM module, but here directly from the source
+ */
+export const fdc3: FDC3 = windowWithFDC3.fdc3 || require('../client/main');
 
-export const fdc3: FDC3 = tmp.fdc3 || require('../client/main');
-
-if (!tmp.fdc3) {
-    tmp.fdc3 = fdc3;
+// Also attach the client to the window, if it isn't already
+if (!windowWithFDC3.fdc3) {
+    windowWithFDC3.fdc3 = fdc3;
 }
