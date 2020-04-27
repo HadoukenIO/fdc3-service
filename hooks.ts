6dc04a0df085923971ab00ff1799a732eb47ef8d
Theo Ottah <48205674+ottah@users.noreply.github.com>
2020-04-27 09:38:05 +0100

    Remove test/provider.json hook

--- hooks.ts
+++ hooks.ts
@@ -4,14 +4,6 @@ import * as path from 'path';
 import {Hook, registerHook} from 'openfin-service-tooling/utils/allowHook';
 
 registerHook(Hook.TEST_MIDDLEWARE, (app) => {
-    // Sneakily return the test directory instead of the default one
-    app.get('/provider/sample-app-directory.json', (req, res) => {
-        const testDirectory = JSON.parse(fs.readFileSync(path.join('res', 'test', 'sample-app-directory.json')).toString());
-
-        res.contentType('application/json');
-        res.json(testDirectory);
-    });
-
     // Delay response. Useful to test timeouts
     // Use as http://localhost:3923/fakeDelay/?t=5000
     app.get('/fakeDelay', (req, res) => {
